60f57bc63594062d821eac9f34057aa8507dd602
Leigh-ann <leighann.gant@gmail.com>
2018-02-05 17:37:29 +0000

    Space model created, listing links spaces form database, database cleaner configured

--- app/models/database_setup.rb
+++ app/models/database_setup.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require_relative 'space'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/dwellbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
--- spec/features/listing_space_spec.rb
+++ spec/features/listing_space_spec.rb
@@ -2,9 +2,15 @@ feature 'a user can list space' do
   scenario 'user can post a space' do
     visit'/spaces/new'
     expect(page).to have_content('Enter space details')
-    fill_in 'space', with: 'My space'
+    fill_in 'name', with: 'My space'
     click_button 'List space'
     expect(page).to have_content("My space listed")
     expect(current_path).to eq '/spaces'
+  end
+  scenario 'posted list increases list count by one' do
+    visit '/spaces/new'
+    fill_in 'name', with: 'My space'
+    click_button 'List space'
+    expect(Space.count).to eq 1
   end
 end
