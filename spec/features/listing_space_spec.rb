de9df910ed9674cf2dd8473adee919e4c68bb392
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-06 10:04:46 +0000

    Add space table to database, test for multiple spaces being listed

--- spec/features/listing_space_spec.rb
+++ spec/features/listing_space_spec.rb
@@ -7,10 +7,22 @@ feature 'a user can list space' do
     expect(page).to have_content("My space listed")
     expect(current_path).to eq '/spaces'
   end
+
   scenario 'posted list increases list count by one' do
     visit '/spaces/new'
     fill_in 'name', with: 'My space'
     click_button 'List space'
     expect(Space.count).to eq 1
+  end
+
+  scenario 'listing multiple spaces' do
+    visit '/spaces/new'
+    fill_in 'name', with: "Lewis's space"
+    click_button 'List space'
+    visit '/spaces/new'
+    fill_in 'name', with: "Leigh-Ann's space"
+    click_button 'List space'
+
+    expect(page).to have_content "Lewis's space" && "Leigh-Ann's space"
   end
 end
