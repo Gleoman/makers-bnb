67f3da9df6cf05f1c3949d8626837630e4227d86
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-07 10:15:48 +0000

    renamed database to use _

--- database_setup.rb
+++ database_setup.rb
@@ -4,6 +4,6 @@ require './app/models/space'
 require './app/models/user'
 require './app/models/availability'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/dwellbnb-#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/dwellbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
